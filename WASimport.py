24ff26766c2b0efc6209fef9e1d2c24ef7357170
Christina Harlow <cmharlow@gmail.com>
2017-05-30 16:13:15 -0700

    pulling last changes from old laptop

--- WASimport.py
+++ WASimport.py
@@ -10,13 +10,35 @@ import datetime
 import json
 import pprint
 import requests
-from requests.auth import HTTPBasicAuth
-import time
+import timeit
 import yaml
+from clint.textui import progress
+import hashlib
 
 
 class WASAPIFile:
-    """Class to handle WASAPI file
+    """Class to handle WASAPI files & metadata taken from REST API response."""
+    def __init__(self, WAS_file):
+        self.locations = WAS_file['locations']
+        self.size = WAS_file['size']
+        self.crawl = WAS_file['crawl']
+        self.sha1 = WAS_file['checksums']['sha1']
+        self.md5 = WAS_file['checksums']['md5']
+        self.crawl_start = WAS_file['crawl-start']
+        self.filetype = WAS_file['filetype']
+        self.filename = WAS_file['filename']
+
+    def grabFiles(self, client):
+        for location in self.locations:
+            print('Downloading file at ' + location)
+            wasfile = client.get(location, stream=True)
+            # To add: checksum validation, add output dir as config
+            with open('sample_data/' + self.filename, 'wb') as fh:
+                total_length = int(wasfile.headers.get('content-length'))
+                for chunk in progress.bar(wasfile.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+                    if chunk:
+                        fh.write(chunk)
+                        fh.flush()
 
 
 def generateFileList(WASAPI_resp, files):
@@ -32,12 +54,13 @@ def generateFileList(WASAPI_resp, files):
     return(files)
 
 
-def getWebData(WASAPI_endpoint, auth, files=None, date_before=None,
+def getWebData(WASAPI_endpoint, client, files=None, date_before=None,
                date_after=None, job_id=None, coll_id=None):
     if 'webdata' not in WASAPI_endpoint:
         WASAPI_URL = WASAPI_endpoint + 'webdata?format=json'
     else:
         WASAPI_URL = WASAPI_endpoint
+    WASAPI_URL = 'https://partner.archive-it.org/wasapi/v1/webdata?format=json&collection=5425&page=5'
     if date_before:
         WASAPI_URL += '&crawl-start-before=' + date_before
     if date_after:
@@ -49,24 +72,20 @@ def getWebData(WASAPI_endpoint, auth, files=None, date_before=None,
 
     print("Trying URL: " + WASAPI_URL)
 
-    resp = requests.get(WASAPI_URL, auth=auth)
+    resp = client.get(WASAPI_URL)
 
     if resp.status_code == requests.codes.ok:
         ait_data = resp.json()
         files = generateFileList(ait_data, files)
         if 'next' in ait_data:
             if ait_data['next']:
-                getWebData(ait_data['next'], auth, files)
+                getWebData(ait_data['next'], client, files)
     else:
+        print(resp.status_code)
+        print(resp.raw())
         print("Error with URL.")
         exit()
-    print(len(files))
     return(files)
-
-
-def authenticate(username, password):
-    auth = HTTPBasicAuth(username, password)
-    return(auth)
 
 
 def main():
@@ -81,9 +100,22 @@ def main():
     username = cfg['development-ait']['username']
     password = cfg['development-ait']['password']
     WASAPI_endpoint = cfg['development-ait']['WASAPI-endpoint']
+    WASAPI_login = cfg['development-ait']['WASAPI-login']
 
-    ait_auth = authenticate(username, password)
-    getWebData(WASAPI_endpoint, ait_auth, args.date_before, args.date_after, args.job_id, args.coll_id)
+    # Authenticate / Create session & CSRF token
+    client = requests.session()
+    client.get(WASAPI_login)
+    csrftoken = client.cookies['csrftoken']
+    login_data = dict(username=username, password=password, csrfmiddlewaretoken=csrftoken, next="/")
+    r = client.post(WASAPI_login, data=login_data, headers=dict(Referer=WASAPI_login))
+
+    # Get list of file dictionaries from webdata API endpoint
+    filelist = getWebData(WASAPI_endpoint, client, None, args.date_before,
+                          args.date_after, args.job_id, args.coll_id)
+    for WAS_file in filelist:
+        WAS_file = WASAPIFile(WAS_file)
+        WAS_file.grabFiles(client)
+
 
 
 if __name__ == '__main__':
